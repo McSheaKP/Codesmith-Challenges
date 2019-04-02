fd4e6e883c5f07a851479ff8b6ba1796b1ff3f4e
Kevin McShea <kevinmcshea@gmail.com>
2019-04-02 14:18:30 -0700

    finsihed oop module in csx

---
+++ oop/developerClass.js
@@ -0,0 +1,33 @@
+// Create a class DeveloperClass that creates objects by extending the PersonClass class. 
+// In addition to having a name property and greet method, 
+// DeveloperClass should have an introduce method. 
+// When called, introduce should log the string "Hello World, my name is [name]".
+
+class PersonClass {
+	constructor(name) {
+    this.name = name;
+	}
+
+	greet() {
+    console.log('hello');
+  }
+}
+
+class DeveloperClass extends PersonClass {
+    super(name){
+        this.name = name;
+        this.greet = greet;
+        
+    }
+    introduce = () => {
+        console.log(`Hello World, my name is ${this.name}`)
+    }
+}
+
+// add code here
+
+const thai = new DeveloperClass('Thai', 32);
+
+// Uncomment these lines to check your work!
+// console.log(thai.name); // -> Logs 'Thai'
+// thai.introduce(); // -> Logs 'Hello World, my name is Thai'
